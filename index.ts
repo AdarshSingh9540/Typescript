9245a4be2c056f8bbc4fcb95d904c662aab85fb5
Adarsh <adarsh9540984202@gmail.com>
2024-03-24 23:47:18 +0530

    add it

--- index.ts
+++ index.ts
@@ -6,3 +6,12 @@ function createUSer(name:String , age:number, income:number){
 }
 
 createUSer("adarsh",5,80000);
+
+let user = {
+    name:String,
+    id:Number,
+    email:String,
+    password:String 
+}
+
+
