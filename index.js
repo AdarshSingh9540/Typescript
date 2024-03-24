537217f3f429889a4a9666010c25a9c4b5c86734
Adarsh <adarsh9540984202@gmail.com>
2024-03-24 23:41:37 +0530

    first

---
+++ index.js
@@ -0,0 +1,5 @@
+var myname = "Adarsh Singh";
+console.log("hi ".concat(myname));
+function createUSer(name, age, income) {
+}
+createUSer("adarsh", 41, 80000);
---
+++ index.ts
@@ -0,0 +1,8 @@
+let myname: string = "Adarsh Singh";
+console.log(`hi ${myname}`);
+
+function createUSer(name:String , age:number, income:number){
+
+}
+
+createUSer("adarsh",5,80000);
