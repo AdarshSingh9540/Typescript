ec002b86d03c1af86d7df9fd81c6d4fbf1e2d4b1
Adarsh <adarsh9540984202@gmail.com>
2024-03-26 20:09:24 +0530

    learn type

---
+++ myObject.js
@@ -0,0 +1,3 @@
+var user1 = { name: "Adarsh", password: "adrash", isActive: true };
+console.log("hi guys")
+console.log(user1);
---
+++ myObject.ts
@@ -0,0 +1,8 @@
+type User = {
+    name:String,
+    password:String,
+    isActive:Boolean
+}
+
+var user1:User = {name:"Adarsh",password:"adrash",isActive:true};
+console.log(user1);
